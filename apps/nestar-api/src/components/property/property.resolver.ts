8b23c360ef97ae9230dc0433dfc2380f040bf3a0
Yakhyobek1997 <yahyobek19979797@gmail.com>
2025-06-18 00:33:45 +0900

    Feat Develop update business

--- apps/nestar-api/src/components/property/property.resolver.ts
+++ apps/nestar-api/src/components/property/property.resolver.ts
@@ -10,32 +10,44 @@ import { Roles } from '../auth/decorators/roles.decorator';
 import { RolesGuard } from '../auth/guards/roles.guard';
 import { WithoutGuard } from '../auth/guards/without.guard';
 import { shapeIntoMongoObjectId } from '../../libs/config';
+import { PropertyUpdate } from '../../libs/dto/property/property.update';
 
 @Resolver()
 export class PropertyResolver {
-  constructor(private readonly propertyService: PropertyService) {}
+	constructor(private readonly propertyService: PropertyService) {}
 
-  @Roles(MemberType.AGENT)
-  @UseGuards(RolesGuard)
-  @Mutation(() => Property)
-  public async createProperty(
-    @Args('input') input: PropertyInput,
-    @AuthMember('_id') memberId: ObjectId,
-  ): Promise<Property> {
-    console.log('Mutation: createProperty');
-    input.memberId = memberId;
-    return await this.propertyService.createProperty(input);
-  }
+	@Roles(MemberType.AGENT)
+	@UseGuards(RolesGuard)
+	@Mutation(() => Property)
+	public async createProperty(
+		@Args('input') input: PropertyInput,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Property> {
+		console.log('Mutation: createProperty');
+		input.memberId = memberId;
+		return await this.propertyService.createProperty(input);
+	}
 
-  @UseGuards(WithoutGuard)
-  @Query((returns) => Property)
-  public async getProperty(
-    @Args('propertyId') input: string,
-    @AuthMember('_id') memberId: ObjectId,
-  ): Promise<Property> {
-    console.log('Query: getProperty');
-    const propertyId = shapeIntoMongoObjectId(input);
-    return await this.propertyService.getProperty(memberId, propertyId);
-  }
+	@UseGuards(WithoutGuard)
+	@Query((returns) => Property)
+	public async getProperty(
+		@Args('propertyId') input: string,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Property> {
+		console.log('Query: getProperty');
+		const propertyId = shapeIntoMongoObjectId(input);
+		return await this.propertyService.getProperty(memberId, propertyId);
+	}
+
+	@Roles(MemberType.AGENT)
+	@UseGuards(RolesGuard)
+	@Mutation(() => Property)
+	public async updateProperty(
+		@Args('input') input: PropertyUpdate,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Property> {
+		console.log('Mutation: updateProperty');
+		input._id = shapeIntoMongoObjectId(input._id);
+		return await this.propertyService.updateProperty(memberId, input);
+	}
 }
-
