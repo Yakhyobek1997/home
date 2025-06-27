ad6bd7fdb60a9f1eac2b7f3a481c74bdfd10f3ca
Yakhyobek1997 <yahyobek19979797@gmail.com>
2025-06-27 19:09:25 +0900

    Feat: create batchRollBack

--- apps/nestar-batch/src/batch.service.ts
+++ apps/nestar-batch/src/batch.service.ts
@@ -2,7 +2,22 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class BatchService {
-  getHello(): string {
-    return 'Welcome to Nestart batch server';
+  public getHello(): string {
+    return 'Welcome to Nestar BATCH Server!';
+  }
+
+  public async batchRollback(): Promise<void> {
+    // TODO: Rollback logic here
+    console.log("batchRollback")
+  }
+
+  public async batchProperties(): Promise<void> {
+    // TODO: Properties batch logic here
+    console.log("batchProperties")
+  }
+
+  public async batchAgents(): Promise<void> {
+    // TODO: Agents batch logic here
+    console.log("batchAgents")
   }
 }
