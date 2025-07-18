243b8dfb2cd5f383a2d7be8e15b1458143eb0aef
Yakhyobek1997 <yahyobek19979797@gmail.com>
2025-07-18 20:25:10 +0900

    Fix

--- apps/nestar-api/src/libs/dto/member/member.input.ts
+++ apps/nestar-api/src/libs/dto/member/member.input.ts
@@ -66,7 +66,7 @@ export class AgentsInquiry {
   limit: number;
 
   @IsOptional()
-  @IsIn([availableAgentSorts])
+  @IsIn(availableAgentSorts)
   @Field(() => String, { nullable: true })
   sort?: string;
 
